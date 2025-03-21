849896aa99a2c6f1d4913b53a85f7ff2eb1eb95c
Yang Yue <202283890018@nuist.edu.cn.com>
2025-03-21 20:23:42 +0800

    Added MultiplyThreeNums.py

---
+++ MultiplyThreeNums.py
@@ -0,0 +1,10 @@
+#Multiply Three Numbers
+#Auther: Yang Yue
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+product = num1 * num2 * num3
+
+print("Their multiplication result is : ", product)
